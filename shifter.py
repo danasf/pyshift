cc476c1e89f1663edb8ad6068fe188322138e7f8
Dana <dsniezko@gmail.com>
2014-07-17 13:53:16 -0400

    added new bits, examples still broken

--- shifter.py
+++ shifter.py
@@ -12,10 +12,14 @@ class Shifter(object):
         # sel1,sel2,sel3 - row select bits 1 MSB, 3 LSB
         # oe - set high to supress display
 
-        def __init__(self,data1,data2,clock,latch,sel1,sel2,sel3,oe):
+        def __init__(self,r1,g1,b1,r2,g2,b2,clock,latch,sel1,sel2,sel3,oe):
                 self.size = 32
-                self.data1 = data1
-                self.data2 = data2
+                self.r1 = r1
+                self.g1 = g1
+                self.b1 = b1
+                self.r2 = r2
+                self.g2 = g2
+                self.b2 = b2
                 self.clock = clock
                 self.latch = latch
                 self.sel1 = sel1
@@ -23,39 +27,63 @@ class Shifter(object):
                 self.sel3 = sel3
                 self.oe = oe
 
-                GPIO.setup(self.data1, GPIO.OUT) # data RED1
-                GPIO.setup(self.data2, GPIO.OUT) # data RED2
+                GPIO.setup(self.r1, GPIO.OUT) # data RED1
+                GPIO.setup(self.r2, GPIO.OUT) # data RED2
+                GPIO.setup(self.g1, GPIO.OUT) 
+                GPIO.setup(self.g2, GPIO.OUT) 
+                GPIO.setup(self.b1, GPIO.OUT) 
+                GPIO.setup(self.b2, GPIO.OUT) 
+
                 GPIO.setup(self.clock, GPIO.OUT) # clock
                 GPIO.setup(self.latch, GPIO.OUT) # latch
 
                 GPIO.setup(self.sel1, GPIO.OUT) # select bit 1
                 GPIO.setup(self.sel2, GPIO.OUT) # select bit 2
                 GPIO.setup(self.sel3, GPIO.OUT) # select bit 3
-                GPIO.setup(self.oe, GPIO.OUT) # select bit 3
+                GPIO.setup(self.oe, GPIO.OUT) # Output Enable
 
                 GPIO.output(self.oe, GPIO.LOW) # set OE LOW
 
                 GPIO.output(self.clock, GPIO.HIGH) # set clock HIGH
-                GPIO.output(self.latch, GPIO.HIGH) # set latch HIGH
+                GPIO.output(self.latch,GPIO.LOW) # set latch LOW
 
         # latch
         def latchData(self):
-                GPIO.output(self.oe, GPIO.HIGH) # set OE LOW
+                GPIO.output(self.oe, GPIO.HIGH) # set OE HIGH
                 GPIO.output(self.latch,GPIO.HIGH)
                 GPIO.output(self.latch,GPIO.LOW)
-                #GPIO.output(self.latch,GPIO.HIGH)
                 GPIO.output(self.oe, GPIO.LOW) # set OE LOW
 
 
-        # send a single bit
-        def sendBit(self,pin,state,clockIn=True):
-                if state is True:
-                        GPIO.output(pin,GPIO.HIGH) # SET dat HIGH
-                else:
-                        GPIO.output(pin,GPIO.LOW) # SET data LOW
+        # set color for top row
+        def setColor(self,half,r,g,b,clockIn=True):
 
+                if half is 0:
+                        if r is True:
+                                GPIO.output(this.r1,GPIO.HIGH) # SET dat HIGH
+                        if g is True:
+                                GPIO.output(this.g1,GPIO.HIGH) # SET dat HIGH
+                        if b is True:
+                                GPIO.output(this.b1,GPIO.HIGH) # SET dat HIGH
+                      
+                elif half is 1:
+                        if r is True:
+                                GPIO.output(this.r2,GPIO.HIGH) # SET dat HIGH
+                        if g is True:
+                                GPIO.output(this.g2,GPIO.HIGH) # SET dat HIGH
+                        if b is True:
+                                GPIO.output(this.b2,GPIO.HIGH) # SET dat HIGH
+   
                 if clockIn:
                         self.clockIn()
+                        
+                        GPIO.output(this.r1,GPIO.LOW) 
+                        GPIO.output(this.g1,GPIO.LOW)
+                        GPIO.output(this.b1,GPIO.LOW) 
+                        GPIO.output(this.r2,GPIO.LOW) 
+                        GPIO.output(this.g2,GPIO.LOW)
+                        GPIO.output(this.b2,GPIO.LOW) 
+
 
         # clock in the bit
         def clockIn(self):
